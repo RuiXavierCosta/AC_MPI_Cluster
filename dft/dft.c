3b0d8521628434dc2a4ad1fb77aca9b9015726ef
Unknown <ruixaviercosta@gmail.com>
2017-12-06 23:16:08 +0000

    esboco de algoritmo de dft

--- dft/dft.c
+++ dft/dft.c
@@ -1,1 +1,17 @@
 #include "dft.h"
+
+
+void dft(double *inreal , double *inimag, double *outreal, double *outimag, int n) {
+
+    for (int k = 0; k < n; k++) {
+        double sumreal = 0;
+        double sumimag = 0;
+        for (int t = 0; t < n; t++) {
+            double angle = 2 * PI * t * k / n;
+            sumreal +=  inreal[t] * cos(angle) + inimag[t] * sin(angle);
+            sumimag += -inreal[t] * sin(angle) + inimag[t] * cos(angle);
+        }
+        outreal[k] = sumreal;
+        outimag[k] = sumimag;
+    }
+}
--- dft/dft.h
+++ dft/dft.h
@@ -9,3 +9,6 @@
 
 #define UM_SEC 1000000000L
 #define PI 3.14159265359
+#define N 64
+
+void dft(double *inreal , double *inimag, double *outreal, double *outimag, int n);
